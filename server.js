1b3d2746dcb5de1f619c515483765b1c60609a39
Tessa Thornton <tessathornton@gmail.com>
2013-08-11 20:24:57 -0400

    point / to index.html

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ app.get('/select', function(req, res){
   res.sendfile('select.html');
 });
 
+app.get('/', function(req, res){
+  res.sendfile('/index.html');
+});
+
 var port = process.env.PORT || 9000;
 app.listen(port, function() {
   console.log("Listening on " + port);
