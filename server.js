bed718bd2c392e48a670bd0cc713241303c588e1
Tessa Thornton <tessathornton@gmail.com>
2013-08-11 20:17:49 -0400

    rearrange files for grunt

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.configure(function(){
   app.set('views', __dirname + '/views');
   app.use(express.bodyParser());
   app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+  app.use(express.static(__dirname + 'dist'));
 });
 
 app.get('/select', function(req, res){
