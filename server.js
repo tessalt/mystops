5e5faa5b840c5d2bff0f8a0cf41fa3ca091547e1
Tessa Thornton <tessathornton@gmail.com>
2013-08-11 20:35:14 -0400

    modify server.js again

--- server.js
+++ server.js
@@ -2,18 +2,9 @@ var express = require("express");
 var app = express();
 
 app.configure(function(){
-  app.set('views', __dirname + '/views');
   app.use(express.bodyParser());
   app.use(app.router);
   app.use(express.static(__dirname + 'dist'));
-});
-
-app.get('/select', function(req, res){
-  res.sendfile('select.html');
-});
-
-app.get('/', function(req, res){
-  res.sendfile('dist/index.html');
 });
 
 var port = process.env.PORT || 9000;
